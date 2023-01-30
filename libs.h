e875bdfa1698d2fe93f4d42f8cfd23b98be78f26
Christian Meng <cmeng@1-D-24.42heilbronn.de>
2023-01-30 10:27:49 +0100

    fixed typo include gnl

--- libs.h
+++ libs.h
@@ -6,7 +6,7 @@
 /*   By: cmeng <cmeng@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/29 22:30:32 by cmeng             #+#    #+#             */
-/*   Updated: 2023/01/29 22:32:32 by cmeng            ###   ########.fr       */
+/*   Updated: 2023/01/30 10:26:33 by cmeng            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,6 @@
 
 # include "libft/libft.h"
 # include "ft_printf/ft_printf.h"
-# include "get_nextline/get_next_line.h"
+# include "get_next_line/get_next_line.h"
 
 #endif
